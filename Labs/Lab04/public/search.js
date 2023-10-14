35bbbb242dbcab5b7fc49eaed2838c28c7ef69e0
Estearle <estearle@gmail.com>
2023-10-14 07:05:44 -0400

    Fix
    Removed some commented code for clarity
    Added a link to search page from result page

--- Labs/Lab04/public/search.js
+++ Labs/Lab04/public/search.js
@@ -5,19 +5,7 @@ window.addEventListener('load', () => {
 function search(){
     let text = document.getElementById("search").value;
     console.log(text);
-    // let xhttp = new XMLHttpRequest()
-    // xhttp.onreadystatechange = function () {
-    //         if (this.readyState == 4 && this.status == 200) {
-    //             let responseItem = JSON.parse(this.responseText)
-    //             console.log(this.responseText);
-    //             document.body.innerHTML = this.responseText;
-    //     }
-    //     //Send a POST request to the server with the information
-    //     //in the JSON object.
-    //     xhttp.open("POST", '/search')
-    //     xhttp.setRequestHeader("Content-Type", "application/json");
-    //     xhttp.send(JSON.stringify({"text":text}));
-    // }
+
     fetch('/search', {
         method: 'POST',
         headers: {
