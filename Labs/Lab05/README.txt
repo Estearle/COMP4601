2f5d4e64f7b43f884b779fd29ce1890dc7ba769b
rachelwongcl <119693648+rachelwongcl@users.noreply.github.com>
2023-10-22 13:36:03 -0400

    README

--- Labs/Lab05/README.txt
+++
@@ -1,2 +0,0 @@
-1. How did you generate the adjacency matrix from the crawled data? 
-2. What effect would lower/higher alpha values have on the results?
--- Labs/Lab05/page-rank.js
+++ Labs/Lab05/page-rank.js
@@ -50,21 +50,9 @@ loadData()
     adjacent = Matrix.zeros(len,len);
     //Initial PageRank vector
     x0 = Matrix.eye(1,len).fill(1/len);
-    // a/N matrix
+    // 1/N matrix
     m = Matrix.ones(len,len).mul(1/len);
-
-    // //adjacent matrix
-    // for (let document of result){
-    //     let curPage = document.title;
-    //     let outgoing = document.outgoing;
-    //     for(let o of outgoing){
-    //         let title = o.split('-').pop().split(".")[0];
-    //         let current = curPage.split('-')[1];
-    //         adjacent.set(current,title,1);
-    //     }
-    // }
-
-    // ***** //
+    
     // Define a mapping from titles to indices
     let titleToIndex = {};
     result.forEach((document, index) => {
@@ -80,7 +68,7 @@ loadData()
             adjacent.set(titleToIndex[curPage], titleToIndex[title], 1);
         }
     }
-    // ***** //
+
 
     //divide each 1 by the number of 1s in that row
     for(let i  = 0 ;i <len ; i++){
