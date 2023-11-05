2f789afbec7d49ceb61caa2ea405c7765fe1915a
Estearle <estearle@gmail.com>
2023-11-05 07:05:18 -0500

    Cleanup
    Removed unnecessary comments and lines
    Change ul to p in detail.pug

--- Assignments/A1/server.js
+++ Assignments/A1/server.js
@@ -331,7 +331,6 @@ loadData()
         app.listen(PORT, () => {
             console.log("Listen on port:", PORT);
 
-            // Your server's base URL
             const serverUrl = `http://localhost:${PORT}`;
 
             // Register with the distributed search engine
@@ -351,14 +350,10 @@ loadData()
             })
             .catch(error => {
                 if (error.response) {
-                    // The request was made and the server responded with a status code
-                    // that falls out of the range of 2xx
                     console.error('Error registering search server:', error.response.data);
                 } else if (error.request) {
-                    // The request was made but no response was received
                     console.error('Error registering search server:', error.request);
                 } else {
-                    // Something happened in setting up the request that triggered an Error
                     console.error('Error', error.message);
                 }
             });
