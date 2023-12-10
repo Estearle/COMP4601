c30225a36775828b0f10e0ce3fef663f9609edde
rachelwongcl <119693648+rachelwongcl@users.noreply.github.com>
2023-12-10 05:23:03 -0500

    user updated

--- Assignments/A2/user.js
+++ Assignments/A2/user.js
@@ -3,6 +3,9 @@ const fs = require("fs").promises;
 
 let allSimilarities = {};
 let NEIGHBOURHOOD_SIZE = 5;
+let THRESOLD_VAL = 0;
+let threshold = true;
+let negatives = false;
 let ratings = {};
 let userAvgs = {};
 let totalPred = 0;
@@ -66,20 +69,15 @@ let errorSum = 0;
                 if (ratings[user][itemA] === 0) continue; //Skip if the user has not rated
                 for (let userB in ratings) {
                     if (userB !== user) {
-                        if(ratings[userB][itemA] === 0) continue;
+                        if (ratings[userB][itemA] === 0) continue;
                         let similarity = calculateSimilarity(ratings, userAvgs, user, userB, itemA);
                         // console.log(user, userB, similarity)
                         if (!allSimilarities[user][itemA]) allSimilarities[user][itemA] = {};
-                        allSimilarities[user][itemA][userB]= similarity;
+                        allSimilarities[user][itemA][userB] = similarity;
                     }
                 }
 
                 let predicted = calculatePredictedRating(user, itemA, allSimilarities, ratings, userAvgs, NEIGHBOURHOOD_SIZE);
-                if (predicted < 1) {
-                    underPred++;
-                } else if (predicted > 5) {
-                    overPred++;
-                }
                 errorSum += Math.abs(predicted - ratings[user][itemA]);
                 totalPred++;
                 // Update the predictions object with the predicted rating, not the original matrix
@@ -90,16 +88,12 @@ let errorSum = 0;
 
         }
 
-
-        // for (let prediction of predictions) {
-        //     let userIndex = prediction.user;
-        //     let itemIndex = prediction.item;
-        //     if (userIndex && itemIndex) {
-        //         ratings[userIndex][itemIndex] = prediction.predictedRating;
-        //     }
-        // }
         console.log("");
-        console.log("User-based, top 2 neighbours");
+        if (threshold) {
+            console.log("User-based, has similarity threshold of " + THRESOLD_VAL +  ", top " + NEIGHBOURHOOD_SIZE + " neighbours");
+        } else {
+            console.log("User-based, no similarity threshold, top " + NEIGHBOURHOOD_SIZE + " neighbours");
+        }
         console.log("Total predictions: " + totalPred);
         console.log("Total under predictions (< 1): " + underPred);
         console.log("Total over predictions (> 5): " + overPred);
@@ -120,9 +114,9 @@ function calculateSimilarity(ratings, userAverages, userA, userB, skipItem) {
     let sumASquared = 0;
     let sumBSquared = 0;
     let count = 0;
-    userAverageUpdate = calculateAdjustedAverage(ratings,userA,skipItem);
+    userAverageUpdate = calculateAdjustedAverage(ratings, userA, skipItem);
     for (let item in ratings[userA]) {
-        if(item === skipItem) continue
+        if (item === skipItem) continue //Skip the user for whom we're calculating similarities
         if (ratings[userA][item] === 0 || ratings[userB][item] === 0) continue; // Skip if either user hasn't rated the item
 
         let ratingA = ratings[userA][item];
@@ -138,7 +132,7 @@ function calculateSimilarity(ratings, userAverages, userA, userB, skipItem) {
     }
 
     if (count === 0 || sumASquared === 0 || sumBSquared === 0) {
-        return 0; 
+        return 0;
     }
 
     return sumProduct / (Math.sqrt(sumASquared) * Math.sqrt(sumBSquared));
@@ -150,38 +144,58 @@ function calculatePredictedRating(userName, itemToPredict, allSimilarities, rati
 
     let sortedSimilarities = [];
     console.log("");
+
+    // Build an array of similarities and corresponding items
     for (let otherUser in allSimilarities[userName][itemToPredict]) {
         let similarity = allSimilarities[userName][itemToPredict][otherUser];
-        if (similarity > 0) {
+        if (!threshold) {// If no threshold then take in all possible values
             sortedSimilarities.push({
                 otherUser: otherUser,
                 similarity: similarity
             });
+        } else {
+            if (THRESOLD_VAL === 0) {
+                if (similarity > THRESOLD_VAL) { // Considering only positive similarities
+                    sortedSimilarities.push({
+                        otherUser: otherUser,
+                        similarity: similarity
+                    });
+                }
+            } else {
+                if (Math.abs(similarity) >= THRESOLD_VAL) { //If a threshold exists,take the absolute similarity for comparison
+                    sortedSimilarities.push({
+                        otherUser: otherUser,
+                        similarity: similarity // Include negative values
+                    })
+                }
+            }
         }
+       
+
     }
-    // console.log("SIM:", sortedSimilarities)
-    // Sort the scores to get the highest ones based on the neighborhood size
-    //ignore negatives
-    sortedSimilarities.sort((a, b) => b.similarity - a.similarity);
-    // include negatives
-    // sortedSimilarities.sort((a, b) => Math.abs(b.similarity) - Math.abs(a.similarity));
-    let adjustedSize = Math.min(sortedSimilarities.length, neighbourhoodSize);
-    let topSimScores = sortedSimilarities.slice(0, adjustedSize);
-    console.log(sortedSimilarities)
+     // Sort by similarity score
+     if (negatives) {
+        sortedSimilarities.sort((a, b) => Math.abs(b.similarity) - Math.abs(a.similarity));
+    } else {
+        sortedSimilarities.sort((a, b) => b.similarity - a.similarity);
+    }
+
     let num = 0;
     let denom = 0;
-    // console.log("TOPSIMSCORE"+topSimScores)
+    let adjustedSize = Math.min(sortedSimilarities.length, neighbourhoodSize);
+    let topSimScores = sortedSimilarities.slice(0, adjustedSize);
+   
 
     console.log(`Predicting for user: ${userName}`);
     console.log(`Predicting for item: ${itemToPredict}`);
     console.log(`Found ${adjustedSize} valid neighbours:`);
 
     topSimScores.forEach((sim, index) => {
-        console.log(`${index + 1}. User${sim.otherUser} sim=${sim.similarity}`);
+        //console.log(`${index + 1}. User${sim.otherUser} sim=${sim.similarity}`);
         num += sim.similarity * (ratings[sim.otherUser][itemToPredict] - userAvgs[sim.otherUser]);
         denom += sim.similarity;
     });
-    let userAverageUpdate = calculateAdjustedAverage(ratings,userName,itemToPredict);
+    let userAverageUpdate = calculateAdjustedAverage(ratings, userName, itemToPredict);
     let predictedRating = userAverageUpdate;
     predictedRating += num / denom;
 
@@ -193,6 +207,17 @@ function calculatePredictedRating(userName, itemToPredict, allSimilarities, rati
         return Number(adjustedAvg.toFixed(2));
     }
     console.log("Initial predicted value: " + Number(predictedRating.toFixed(2)));
+    if (num / denom < 1) {
+        underPred++;
+        console.log("Final Predicted Value: " + 1);
+        sumNeighbour+=adjustedSize;
+        return (1);
+    } else if (num / denom > 5) {
+        overPred++;
+        console.log("Final Predicted Value: " + 5);
+        sumNeighbour+=adjustedSize;
+        return (5);
+    }
     console.log("Final Predicted Value: " + Number(predictedRating.toFixed(2)));
     sumNeighbour += adjustedSize;
 
